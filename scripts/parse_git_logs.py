da3712f3d543f4af3fb58f7e25e57dac921a077e
hcalex1 <alex.hoangcao@hotmail.com>
2023-03-28 10:17:32 -0400

    Added input and output arguments to parse_git_logs.py

--- scripts/parse_git_logs.py
+++ scripts/parse_git_logs.py
@@ -1,8 +1,16 @@
-GIT_LOGS_PATH = "./inputs-outputs/git_logs.txt"
-OUTPUT_PATH   = "./inputs-outputs/commits.csv"
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', type=str)
+parser.add_argument('-o', type=str)
+
+args = parser.parse_args()
+
+git_logs_path = args.i
+output_path   = args.o
 
 output_content = "PathFile,Commit hash,Commit timestamp,Subject\n"
-with open(GIT_LOGS_PATH, "r") as log_file:
+with open(git_logs_path, "r") as log_file:
 
     for line in log_file:
         key   = line[0]
@@ -17,5 +25,5 @@ with open(GIT_LOGS_PATH, "r") as log_file:
         elif key == 'M':
             output_content += f"{value},{hash},{timestamp},{subject}\n"
 
-with open(OUTPUT_PATH, "w") as output_file:
+with open(output_path, "w") as output_file:
     output_file.write(output_content)
